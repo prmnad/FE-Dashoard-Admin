e2df24caea3d288916ad70fb02c047d9eb8764b3
adprm <aditiyaprmn00@gmail.com>
2020-09-08 10:47:37 +0700

    done

---
+++ js/admin.js
@@ -0,0 +1,14 @@
+// tool tip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+// smootch click href
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
